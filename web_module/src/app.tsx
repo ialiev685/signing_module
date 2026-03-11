5072ff3d7f95d4042d3893194fd016cc96f00b8a
ilfat aliev <ialiev685@gmail.com>
2026-03-11 16:00:00 +0300

    установил маршрутизатор

--- web_module/src/app.tsx
+++ web_module/src/app.tsx
@@ -2,8 +2,34 @@ import { Layout } from "@components/layout";
 import "./fonts/fonts.css";
 import "./app.css";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Verification } from "@pages/verification";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: (
+      <div>
+        <h1>Ошибка</h1>
+        <p>Что-то пошло не так</p>
+      </div>
+    ),
+    children: [
+      {
+        path: "verificationSignature",
+        element: <Verification />,
+      },
+      {
+        path: "rootCertificate",
+        element: <></>,
+      },
+    ],
+  },
+]);
+
 const App = () => {
-  return <Layout>hello world</Layout>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
