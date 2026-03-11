7ba7e96a8f8f71e7c7765fcfe8c86d5190554dc8
ilfat aliev <ialiev685@gmail.com>
2026-03-11 14:00:31 +0300

    инициация веб приложения

---
+++ web_module/src/app.tsx
@@ -0,0 +1,9 @@
+import { Layout } from "@components/layout";
+import "./fonts/fonts.css";
+import "./app.css";
+
+const App = () => {
+  return <Layout>hello world</Layout>;
+};
+
+export default App;
