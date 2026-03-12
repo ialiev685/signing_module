7c71b4c125d2cbb700d57c1bc718e3393ddf96e1
ilfat aliev <ialiev685@gmail.com>
2026-03-12 17:09:41 +0300

    добавил кнопку верификации

--- web_module/src/pages/verification.tsx
+++ web_module/src/pages/verification.tsx
@@ -1,14 +1,40 @@
+// import { api } from "@/services/client";
 import { UploadFile } from "@components/upload-file";
-import { Title, Flex } from "@mantine/core";
+import { Title, Flex, Button, SimpleGrid } from "@mantine/core";
+import { useState } from "react";
 
 export const Verification = () => {
+  const [documents, setDocuments] = useState<File[] | undefined>(undefined);
+  const [signatures, setSignatures] = useState<File[] | undefined>(undefined);
+
+  const handleVerification = () => {
+    // api.verifySignatureApiV1VerifySignaturePost({});
+  };
+
   return (
-    <Flex gap={24} direction="column">
+    <Flex gap={24} direction="column" align="flex-start">
       <Title order={4}>Проверка открепленной подписи</Title>
-      <UploadFile
-        title="Загрузите документ"
-        accept={["doc", "jpeg", "pdf", "png", "xls", "xlsx"]}
-      />
+      <SimpleGrid cols={2}>
+        <UploadFile
+          files={documents}
+          title="Загрузите документ"
+          accept={["doc", "jpeg", "pdf", "png", "xls", "xlsx"]}
+          onChange={setDocuments}
+        />
+        <UploadFile
+          files={signatures}
+          title="Загрузите подпись"
+          accept={["p7b", "p7s", "sig"]}
+          onChange={setSignatures}
+        />
+      </SimpleGrid>
+      <Button
+        disabled={!documents || !signatures}
+        color="teal"
+        onClick={handleVerification}
+      >
+        Проверить
+      </Button>
     </Flex>
   );
 };
