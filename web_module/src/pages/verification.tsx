e74790e697d0101899baeb91de5b0b0a7ef60b41
ilfat aliev <ialiev685@gmail.com>
2026-03-14 10:14:38 +0300

    интегрировал карточку сертификата

--- web_module/src/pages/verification.tsx
+++ web_module/src/pages/verification.tsx
@@ -1,41 +1,60 @@
+import { CardInfo } from "@/components/card-info";
+import type { ResponseDataModelVerificationModel } from "@/services/Api";
 import { api } from "@/services/client";
 import { UploadFile } from "@components/upload-file";
 import { Title, Flex, Button, SimpleGrid } from "@mantine/core";
 import { useState } from "react";
+import { getCertificateContentBar } from "./lib";
 
 export const Verification = () => {
   const [documents, setDocuments] = useState<File[] | undefined>(undefined);
   const [signatures, setSignatures] = useState<File[] | undefined>(undefined);
-  // const [decodedData, setDecodedData] = useState<File[] | undefined>(undefined);
+  const [decodedData, setDecodedData] =
+    useState<ResponseDataModelVerificationModel["data"]>(undefined);
+  const [error, setError] = useState<string | undefined>(undefined);
 
   const handleVerification = async () => {
     if (!documents?.length || !signatures?.length) return;
 
-    const { data } = await api.verifySignatureApiV1VerifySignaturePost({
-      detached_signature: signatures[0],
-      document: documents[0],
-    });
-
-    console.log("result", data);
+    try {
+      const { data } = await api.verifySignatureApiV1VerifySignaturePost({
+        detached_signature: signatures[0],
+        document: documents[0],
+      });
+      setDecodedData(data.data);
+    } catch (error: unknown) {
+      if (typeof error === "string") {
+        setError(error);
+      }
+    }
   };
 
+  const content = decodedData ? (
+    <CardInfo
+      title="Информация о сертификате"
+      contentBar={getCertificateContentBar(decodedData)}
+    />
+  ) : (
+    <SimpleGrid cols={2}>
+      <UploadFile
+        files={documents}
+        title="Загрузите документ"
+        accept={["doc", "jpeg", "pdf", "png", "xls", "xlsx"]}
+        onChange={setDocuments}
+      />
+      <UploadFile
+        files={signatures}
+        title="Загрузите подпись"
+        accept={["p7b", "p7s", "sig"]}
+        onChange={setSignatures}
+      />
+    </SimpleGrid>
+  );
+
   return (
-    <Flex gap={24} direction="column" align="flex-start">
+    <Flex gap={24} direction="column">
       <Title order={4}>Проверка открепленной подписи</Title>
-      <SimpleGrid cols={2}>
-        <UploadFile
-          files={documents}
-          title="Загрузите документ"
-          accept={["doc", "jpeg", "pdf", "png", "xls", "xlsx"]}
-          onChange={setDocuments}
-        />
-        <UploadFile
-          files={signatures}
-          title="Загрузите подпись"
-          accept={["p7b", "p7s", "sig"]}
-          onChange={setSignatures}
-        />
-      </SimpleGrid>
+      {content}
       <Button
         disabled={!documents || !signatures}
         color="teal"
