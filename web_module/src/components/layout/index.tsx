5baafe9a8a55dd2e6fc675bddebba037463abd4b
ilfat aliev <ialiev685@gmail.com>
2026-03-15 15:29:28 +0300

    стилизовал ссылки

--- web_module/src/components/layout/index.tsx
+++ web_module/src/components/layout/index.tsx
@@ -1,14 +1,17 @@
 import { AppShell, List, Title, useMantineTheme, Box } from "@mantine/core";
 import { NavLink, Outlet } from "react-router-dom";
 import { navItems } from "./config";
+import styles from "./styles.module.css";
 
 export const Layout = () => {
   const { colors } = useMantineTheme();
 
   return (
     <AppShell header={{ height: 60 }}>
-      <AppShell.Header p={12} bg="cyan">
-        <Title order={2}>Модуль подписания</Title>
+      <AppShell.Header p={12} bg={colors.gray[8]}>
+        <Title order={2} c="#FFF">
+          Модуль подписания
+        </Title>
       </AppShell.Header>
 
       <AppShell.Navbar py={24} px={12} w={330}>
@@ -16,13 +19,10 @@ export const Layout = () => {
           {navItems.map(({ title, link }) => (
             <List.Item key={title}>
               <NavLink
+                className={({ isActive }) =>
+                  [styles.link, isActive ? styles.active : ""].join(" ")
+                }
                 to={link}
-                style={({ isActive }) => ({
-                  textDecoration: "none",
-                  color: isActive ? "#228be6" : "inherit",
-                  fontWeight: isActive ? 600 : 400,
-                  transition: "color 0.2s ease",
-                })}
               >
                 {title}
               </NavLink>
