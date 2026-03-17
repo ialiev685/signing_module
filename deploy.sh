bbeee331e48b976a1fa6b68ffb8f51708dbffbec
ilfat aliev <ialiev685@gmail.com>
2026-03-17 13:00:04 +0300

    обновил deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ docker-compose up -d --build
 cd web_module
 export NODE_ENV=production
 
-yarn install --frozen-lockfile
+yarn install
 yarn build
 
 sudo mkdir -p /var/www/signature-verification.net.ru
